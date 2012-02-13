dc6a49868d35f61563893165ca260dc05183e070
vincent.blouin@gmail.com <vincent.blouin@gmail.com@40667f71-9be7-da36-2ea2-2722c72b7549>
2012-02-13 03:25:31 +0000

    refactor

--- src/main/java/org/triple_brain/module/json/drawn_graph/PointJSONFields.java
+++
@@ -1,9 +0,0 @@
-package org.triple_brain.model.json.drawn_graph;
-
-/**
- * @author Vincent Blouin
- */
-public class PointJSONFields {
-    public static final String X = "x";
-    public static final String Y = "y";
-}
--- src/test/java/org/triple_brain/module/model/UserTest.java
+++ src/test/java/org/triple_brain/module/model/UserTest.java
@@ -1,4 +1,4 @@
-package org.triple_brain.module;
+package org.triple_brain.module.model;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.DigestUtils;
