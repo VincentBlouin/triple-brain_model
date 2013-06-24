1c14b0272df4436e61ceb72628bfc2be354cf464
Vincent Blouin <vincent.blouin@gmail.com>
2013-06-24 17:05:27 -0400

    added creation and last modification dates to graph elements

--- src/main/java/org/triple_brain/module/model/graph/GraphElement.java
+++ src/main/java/org/triple_brain/module/model/graph/GraphElement.java
@@ -1,11 +1,14 @@
 package org.triple_brain.module.model.graph;
 
+import org.joda.time.DateTime;
 import org.triple_brain.module.model.User;
 
 /**
  * Copyright Mozilla Public License 1.1
  */
 public interface GraphElement {
+    public DateTime creationDate();
+    public DateTime lastModificationDate();
     public String id();
     public String label();
     public void label(String label);
