b352263882ffaf0f9b6f32efd82934f6efb6ac73
Vincent Blouin <vincent.blouin@gmail.com>
2013-09-17 16:53:38 -0400

    avoiding special characters in service urls

--- src/main/java/org/triple_brain/module/model/graph/GraphTransactional.java
+++ src/main/java/org/triple_brain/module/model/graph/GraphTransactional.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-public @interface GraphElementIdentifier {
+public @interface GraphTransactional {
 }
