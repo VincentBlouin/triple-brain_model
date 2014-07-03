217d8d6422d703030b58a74e18b92a958ed587c9
Vincent Blouin <vincent.blouin@gmail.com>
2014-07-03 08:58:08 -0400

    test jenkins

--- src/main/java/org/triple_brain/module/model/graph/vertex/VertexPojo.java
+++ src/main/java/org/triple_brain/module/model/graph/vertex/VertexPojo.java
@@ -17,6 +17,8 @@ import java.util.*;
  */
 public class VertexPojo implements Vertex {
 
+    //test jenkins;
+
     private GraphElementPojo graphElement;
     private Integer numberOfConnectedEdges;
     private Map<URI, VertexInSubGraphPojo> includedVertices;
