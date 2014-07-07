19c0e9274b4ea06559c3c9e3cb59081642d9a16b
Vincent Blouin <vincent.blouin@gmail.com>
2014-07-07 08:31:19 -0400

    images as json

--- src/main/java/org/triple_brain/module/model/json/ImageJson.java
+++ src/main/java/org/triple_brain/module/model/json/ImageJson.java
@@ -5,6 +5,7 @@ package org.triple_brain.module.model.json;
 */
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.triple_brain.module.model.Image;
@@ -14,6 +15,14 @@ import java.util.Set;
 public class ImageJson {
 
     private static Gson gson = new Gson();
+
+    public static Set<Image> fromJson(String json) {
+        return gson.fromJson(
+                json,
+                new TypeToken<Set<Image>>() {
+                }.getType()
+        );
+    }
 
     public static JSONArray toJsonArray(Set<Image> images) {
         try {
