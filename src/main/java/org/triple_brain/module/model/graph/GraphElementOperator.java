167ec7b6b0315445ebf30e99306b4615a2ce9be9
Vincent Blouin <vincent.blouin@gmail.com>
2014-12-15 08:37:37 -0500

    accept suggestion performance

--- src/main/java/org/triple_brain/module/model/graph/GraphElementOperator.java
+++ src/main/java/org/triple_brain/module/model/graph/GraphElementOperator.java
@@ -4,10 +4,6 @@
 
 package org.triple_brain.module.model.graph;
 
-import org.triple_brain.module.model.FriendlyResource;
-
-import java.util.Map;
-
 public interface GraphElementOperator extends GraphElement, IdentificationOperator {
     void remove();
     void removeIdentification(Identification type);
