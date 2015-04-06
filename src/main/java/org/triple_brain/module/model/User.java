4e10ca9d2f176680876b8b5caeba0e0cfdf51a13
Vincent Blouin <vincent.blouin@gmail.com>
2015-04-06 12:47:47 -0400

    progress forget password

--- src/main/java/org/triple_brain/module/model/User.java
+++ src/main/java/org/triple_brain/module/model/User.java
@@ -47,7 +47,6 @@ public class User {
 
     public User password(String password) {
         if (password != null) {
-
             salt = UUID.randomUUID().toString();
             passwordHash = encode(password);
         }
