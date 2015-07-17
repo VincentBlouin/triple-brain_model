e684476f857458dc4af0d9735a435dfd11a8818d
Vincent Blouin <vincent.blouin@gmail.com>
2015-07-17 15:47:31 -0400

    resources related to an identification are now like search results ... in the backend

--- src/main/java/guru/bubl/module/model/IdentifiedTo.java
+++ src/main/java/guru/bubl/module/model/IdentifiedTo.java
@@ -4,13 +4,13 @@
 
 package guru.bubl.module.model;
 
-import guru.bubl.module.model.graph.FriendlyResourcePojo;
 import guru.bubl.module.model.graph.Identification;
+import guru.bubl.module.model.search.GraphElementSearchResult;
 
 import java.util.Set;
 
 public interface IdentifiedTo {
-    Set<FriendlyResourcePojo> getForIdentificationAndUser(
+    Set<GraphElementSearchResult> getForIdentificationAndUser(
             Identification identification,
             User user
     );
