a09dcbe413f632f34efe2320fc8f3d61262c7ad4
Vincent Blouin <vincent.blouin@gmail.com>
2015-11-11 12:44:08 -0500

    test failure

--- src/main/java/guru/bubl/module/model/search/IdentificationSearchResult.java
+++ src/main/java/guru/bubl/module/model/search/IdentificationSearchResult.java
@@ -6,20 +6,25 @@ package guru.bubl.module.model.search;
 
 import guru.bubl.module.model.graph.GraphElementPojo;
 
+import java.net.URI;
+
 public class IdentificationSearchResult implements GraphElementSearchResult {
 
     private GraphElementPojo graphElement;
     private Integer nbReferences;
     private String type;
+    private URI externalUri;
 
 
     public IdentificationSearchResult(
             GraphElementPojo graphElement,
             String type,
+            URI externalUri,
             Integer nbReferences
     ){
         this.graphElement = graphElement;
         this.type = type;
+        this.externalUri = externalUri;
         this.nbReferences = nbReferences;
     }
 
@@ -43,5 +48,9 @@ public class IdentificationSearchResult implements GraphElementSearchResult {
 
     public Integer getNbReferences(){
         return nbReferences;
+    }
+
+    public URI getExternalUri(){
+        return this.externalUri;
     }
 }
