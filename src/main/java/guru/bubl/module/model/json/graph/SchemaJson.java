29f4c38b8be4c18ae3f37045b62fff5f7e55b57d
Vincent Blouin <vincent.blouin@gmail.com>
2016-03-27 16:40:25 -0400

    schema list

--- src/main/java/guru/bubl/module/model/json/graph/SchemaJson.java
+++ src/main/java/guru/bubl/module/model/json/graph/SchemaJson.java
@@ -5,10 +5,17 @@
 package guru.bubl.module.model.json.graph;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import guru.bubl.module.model.graph.IdentificationPojo;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import guru.bubl.module.model.graph.schema.Schema;
 import guru.bubl.module.model.graph.schema.SchemaPojo;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class SchemaJson {
     private static Gson gson = new Gson();
@@ -27,6 +34,22 @@ public class SchemaJson {
         return gson.fromJson(
                 json,
                 SchemaPojo.class
+        );
+    }
+
+    public static Set<SchemaPojo> listFromJson(String json){
+        return gson.fromJson(
+                json,
+                new TypeToken<Set<SchemaPojo>>() {
+                }.getType()
+        );
+    }
+
+    public static String jsonFromList(Set<SchemaPojo> schemaList){
+        return gson.toJson(
+                schemaList,
+                new TypeToken<Set<SchemaPojo>>() {
+                }.getType()
         );
     }
 }
