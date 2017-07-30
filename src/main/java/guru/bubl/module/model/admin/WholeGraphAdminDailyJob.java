79fabd1aeaf8ec5200bf8e0f4eeeebf5e16edf18
Vincent Blouin <vincent.blouin@gmail.com>
2017-07-30 11:03:59 -0400

    try schedule task

--- src/main/java/guru/bubl/module/model/admin/WholeGraphAdminDailyJob.java
+++ src/main/java/guru/bubl/module/model/admin/WholeGraphAdminDailyJob.java
@@ -4,19 +4,15 @@
 
 package guru.bubl.module.model.admin;
 
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-
 import javax.inject.Inject;
 
 @javax.inject.Singleton
-@org.nnsoft.guice.guartz.Scheduled(jobName = "admin task", cronExpression = "0 0 2 * * ?")
-public class WholeGraphAdminDailyTask implements org.quartz.Job {
+public class WholeGraphAdminDailyJob{
 
     @Inject
     private WholeGraphAdmin wholeGraphAdmin;
 
-    public void execute(JobExecutionContext context) throws JobExecutionException {
+    public void execute(){
         wholeGraphAdmin.reindexAll();
         wholeGraphAdmin.refreshNumberOfReferencesToAllIdentifications();
         wholeGraphAdmin.removeMetasHavingZeroReferences();
