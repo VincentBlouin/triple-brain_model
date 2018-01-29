83ec2dcf5c15b0f840cc274f310c1cec60a27e20
Vincent Blouin <vincent.blouin@gmail.com>
2018-01-29 17:31:27 -0500

    remove self identifier

--- src/main/java/guru/bubl/module/model/graph/identification/IdentificationOperator.java
+++ src/main/java/guru/bubl/module/model/graph/identification/IdentificationOperator.java
@@ -6,8 +6,11 @@ package guru.bubl.module.model.graph.identification;
 
 import guru.bubl.module.model.graph.FriendlyResourceOperator;
 
+import java.net.URI;
+
 public interface IdentificationOperator extends Identifier, FriendlyResourceOperator {
     void setNbReferences(Integer nb);
+    void setExternalResourceUri(URI uri);
     IdentifierPojo buildPojo();
     void mergeTo(Identifier identifier);
 }
