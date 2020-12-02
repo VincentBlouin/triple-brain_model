4bf4b2d19957bdaadecec22b9c46795fcedb49dd
Vincent Blouin <vincent.blouin@gmail.com>
2020-12-02 14:31:43 -0500

    copy in share levels

--- src/main/java/guru/bubl/module/model/graph/tree_copier/TreeCopier.java
+++ src/main/java/guru/bubl/module/model/graph/tree_copier/TreeCopier.java
@@ -1,20 +1,35 @@
 package guru.bubl.module.model.graph.tree_copier;
 
 import guru.bubl.module.model.User;
+import guru.bubl.module.model.graph.ShareLevel;
 import guru.bubl.module.model.graph.Tree;
 
 import java.net.URI;
 import java.util.Map;
 
 public interface TreeCopier {
+
     Map<URI, URI> copyTreeOfUser(
             Tree tree,
             User copiedUser
+    );
+
+    Map<URI, URI> copyTreeOfUserInShareLevel(
+            Tree tree,
+            User copiedUser,
+            ShareLevel shareLevel
     );
 
     Map<URI, URI> copyTreeOfUserWithNewParentUri(
             Tree tree,
             User copiedUser,
             URI newParentUri
+    );
+
+    Map<URI, URI> copyTreeOfUserWithNewParentUriInShareLevel(
+            Tree tree,
+            User copiedUser,
+            URI newParentUri,
+            ShareLevel shareLevel
     );
 }
