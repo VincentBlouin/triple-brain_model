6996219a1c4f8e0e172e56dff6b037f0834926c7
Vincent Blouin <vincent.blouin@gmail.com>
2020-12-25 15:14:17 -0500

    Notifications uri

--- src/main/java/guru/bubl/module/model/notification/Notification.java
+++ src/main/java/guru/bubl/module/model/notification/Notification.java
@@ -4,19 +4,24 @@ import java.net.URI;
 import java.util.Date;
 
 public class Notification {
+    private URI uri;
     private URI rootUri;
     private URI watchUri;
     private Date creationDate;
     private String action;
-
     private String watchLabel;
 
-    public Notification(URI rootUri, URI watchUri, Date creationDate, String action, String watchLabel) {
+    public Notification(URI uri, URI rootUri, URI watchUri, Date creationDate, String action, String watchLabel) {
+        this.uri = uri;
         this.rootUri = rootUri;
         this.watchUri = watchUri;
         this.creationDate = creationDate;
         this.action = action;
         this.watchLabel = watchLabel;
+    }
+
+    public URI getUri() {
+        return uri;
     }
 
     public URI getRootUri() {
